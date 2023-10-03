2e5da3f8c5295d97f6bb3fb75832bb3ccdb41ccf
LadikBeg <vladbehim@gmail.com>
2023-10-03 17:52:22 +0300

    emoji to goal

---
+++ Financial Management/View/Goal/EmojiView.swift
@@ -0,0 +1,18 @@
+//
+//  EmojiView.swift
+//  Financial Management
+//
+//  Created by Vladyslav Behim on 03.10.2023.
+//
+
+import SwiftUI
+
+struct EmojiView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmojiView()
+}
