8157d3bba24e233d51a77328058ff3ad618d9301
LadikBeg <vladbehim@gmail.com>
2023-10-05 11:29:28 +0300

    goal(changed text color, added emoji to history, added the ability to select wallet type when put money to goal)

---
+++ Financial Management/View/FuturePayment/AddFuturePaymentView.swift
@@ -0,0 +1,18 @@
+//
+//  AddFuturePaymentView.swift
+//  Financial Management
+//
+//  Created by Vladyslav Behim on 05.10.2023.
+//
+
+import SwiftUI
+
+struct AddFuturePaymentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddFuturePaymentView()
+}
