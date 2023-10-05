58e22eb82512d406af996523c01f3d0d02a1ea92
LadikBeg <vladbehim@gmail.com>
2023-10-05 20:54:36 +0300

    future payment

---
+++ Financial Management/View/FuturePayment/FuturePayment.swift
@@ -0,0 +1,18 @@
+//
+//  FuturePayment.swift
+//  Financial Management
+//
+//  Created by Vladyslav Behim on 05.10.2023.
+//
+
+import SwiftUI
+
+struct FuturePayment: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FuturePayment()
+}
